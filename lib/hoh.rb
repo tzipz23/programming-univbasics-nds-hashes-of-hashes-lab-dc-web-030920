27c8ab8f74f5145a4fa1bc090ca1e7f0438acd9d
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-02 05:08:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,69 +1,28 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
 
 def naming_system
-  
-  naming_system = {
-    label: "Kingdom",
-    sub_category: {
-       label: "Phylum",
-      sub_category: {
-         label: "Class"
-      }
+   family = {
+  :label => "Family",
+  :sub_category => {
+    :label => "Genus",
+    :sub_category => {
+        :label => "Species",
+  :sub_category => nil
     }
-  },
-  {
-   label: "Order"
- },
- {
-     label: "Family",
-    sub_category: {
-       label: "Genus",
-     }
-   },
-  {
-     label: "Species",
-     sub_category: nil
-   }
-  
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
+  }
+}
+   order = {
+   :label => "Order", 
+   :sub_category => family
+ }
+    domain = {
+  :label => "Kingdom",
+   :sub_category => {
+     :label => "Phylum",
+     :sub_category => {
+       :label => "Class",
+        :sub_category => order 
+    }
+  }
+}
+  domain
 end
