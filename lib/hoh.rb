126f2273c6090f011f929d84c7571c3eca412bcc
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-02 05:06:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,6 +31,30 @@
 # }
 
 def naming_system
+  
+  naming_system = {
+    label: "Kingdom",
+    sub_category: {
+       label: "Phylum",
+      sub_category: {
+         label: "Class"
+      }
+    }
+  },
+  {
+   label: "Order"
+ },
+ {
+     label: "Family",
+    sub_category: {
+       label: "Genus",
+     }
+   },
+  {
+     label: "Species",
+     sub_category: nil
+   }
+  
   # Remember:
   #  Kingdom
   #  Phylum
